54d2de71c997ae2ec6a0729e2fc6111a908a8279
Ernest Witkowski <140439624+EKW1993@users.noreply.github.com>
2024-02-25 17:50:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
- 
-class Config:
-    DEBUG = True
-    SECRET_KEY = 'super-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+import os
+
+class Config: 
+    DEBUG = True 
+    SECRET_KEY = 'super-secret-key' 
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db' 
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin") 
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
